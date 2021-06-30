dd79823566868e0de67b949fbbf719c748d7824c
unknown <11039083404@est204378.codevasf.gov.br>
2021-06-30 08:59:27 -0300

    Merge Changes

--- resources/views/registro.blade.php
+++ resources/views/registro.blade.php
@@ -139,12 +139,19 @@
                                 <div class="col-lg-2">
                                     <div class="form-group">
                                         <label class="form-control-label" for="numero">Numero</label>
-                                        <input type="text" value="{{ old('numero') }}" id="numero" name="numero" class="form-control" placeholder="Nº" required>
+                                        <input type="number" value="{{ old('numero') }}" id="numero" name="numero" class="form-control" placeholder="Nº" required>
                                         <div class="invalid-feedback">Campo de preenchimento obrigatório</div>
                                     </div>
                                 </div>
 
                                 <div class="col-lg-5">
+                                    <div class="form-group">
+                                        <label class="form-control-label" for="complemento">Complemento</label>
+                                        <input type="text" value="{{ old('complemento') }}" id="complemento" name="complemento" class="form-control" placeholder="Complemento">
+                                    </div>
+                                </div>
+
+                                <div class="col-lg-7">
                                     <div class="form-group">
                                         <label class="form-control-label" for="bairro">Bairro</label>
                                         <input type="text" value="{{ old('bairro') }}" id="bairro" name="bairro" class="form-control" placeholder="Bairro" required>
@@ -179,27 +186,27 @@
                                 </div> 
                                 <div class="col-sm-12 col-md-12 col-lg-12">
                                     <label class="checkbox">
-                                        <input type="checkbox" required/>
+                                        <input type="checkbox" required="">
                                         <svg viewBox="0 0 21 18">
                                             <symbol id="tick-path" viewBox="0 0 21 18" xmlns="http://www.w3.org/2000/svg">
-                                                <path d="M5.22003 7.26C5.72003 7.76 7.57 9.7 8.67 11.45C12.2 6.05 15.65 3.5 19.19 1.69" fill="none" stroke-width="2.25" stroke-linecap="round" stroke-linejoin="round" />
+                                                <path d="M5.22003 7.26C5.72003 7.76 7.57 9.7 8.67 11.45C12.2 6.05 15.65 3.5 19.19 1.69" fill="none" stroke-width="2.25" stroke-linecap="round" stroke-linejoin="round"></path>
                                             </symbol>
                                             <defs>
                                                 <mask id="tick">
-                                                    <use class="tick mask" href="#tick-path" />
+                                                    <use class="tick mask" href="#tick-path"></use>
                                                 </mask>
                                             </defs>
-                                            <use class="tick" href="#tick-path" stroke="currentColor" />
-                                            <path fill="white" mask="url(#tick)" d="M18 9C18 10.4464 17.9036 11.8929 17.7589 13.1464C17.5179 15.6054 15.6054 17.5179 13.1625 17.7589C11.8929 17.9036 10.4464 18 9 18C7.55357 18 6.10714 17.9036 4.85357 17.7589C2.39464 17.5179 0.498214 15.6054 0.241071 13.1464C0.0964286 11.8929 0 10.4464 0 9C0 7.55357 0.0964286 6.10714 0.241071 4.8375C0.498214 2.39464 2.39464 0.482143 4.85357 0.241071C6.10714 0.0964286 7.55357 0 9 0C10.4464 0 11.8929 0.0964286 13.1625 0.241071C15.6054 0.482143 17.5179 2.39464 17.7589 4.8375C17.9036 6.10714 18 7.55357 18 9Z" />
+                                            <use class="tick" href="#tick-path" stroke="currentColor"></use>
+                                            <path fill="white" mask="url(#tick)" d="M18 9C18 10.4464 17.9036 11.8929 17.7589 13.1464C17.5179 15.6054 15.6054 17.5179 13.1625 17.7589C11.8929 17.9036 10.4464 18 9 18C7.55357 18 6.10714 17.9036 4.85357 17.7589C2.39464 17.5179 0.498214 15.6054 0.241071 13.1464C0.0964286 11.8929 0 10.4464 0 9C0 7.55357 0.0964286 6.10714 0.241071 4.8375C0.498214 2.39464 2.39464 0.482143 4.85357 0.241071C6.10714 0.0964286 7.55357 0 9 0C10.4464 0 11.8929 0.0964286 13.1625 0.241071C15.6054 0.482143 17.5179 2.39464 17.7589 4.8375C17.9036 6.10714 18 7.55357 18 9Z"></path>
                                         </svg>
                                         <svg class="lines" viewBox="0 0 11 11">
-                                            <path d="M5.88086 5.89441L9.53504 4.26746" />
-                                            <path d="M5.5274 8.78838L9.45391 9.55161" />
-                                            <path d="M3.49371 4.22065L5.55387 0.79198" />
+                                            <path d="M5.88086 5.89441L9.53504 4.26746"></path>
+                                            <path d="M5.5274 8.78838L9.45391 9.55161"></path>
+                                            <path d="M3.49371 4.22065L5.55387 0.79198"></path>
                                         </svg>
                                     </label>
                                     <span style="float:left; margin: -1.9em 0em 0em 1.8em;">Declaro que li e aceito os <a href="javascript:void(0)" data-toggle="modal" data-target="#termos" style="display: inline-block">Termos de uso.</a></span>
-                                </div> 
+                                </div>
                             </div>
                             <br>
                             <div class="row justify-content-center align-items-center">
--- resources/views/templates/site.blade.php
+++ resources/views/templates/site.blade.php
@@ -15,7 +15,7 @@
 @yield('content')
 
 <script src="{{ asset('js/app.js') }}"></script>
-<script src="//cdn.jsdelivr.net/npm/sweetalert2@10"></script>
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@10"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.16/jquery.mask.min.js"></script>
 <script src="{{ asset('js/buscarcep.js') }}"></script>
